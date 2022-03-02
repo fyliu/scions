abaf852fd549d4b80d1361edbe874f05b375d0e1
Fang Yi Liu <fyliu@users.noreply.github.com>
2022-03-02 15:29:40 -0800

    Black changes

--- app/scionexchange/wsgi.py
+++ app/scionexchange/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scionexchange.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scionexchange.settings")
 
 application = get_wsgi_application()
